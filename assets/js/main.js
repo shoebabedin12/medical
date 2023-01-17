447d7b8f840f3f1d56d191a8ce38a6787f686c1d
shoebabedin12 <shoebabedin12@gmail.com>
2023-01-17 20:35:52 +0600

    new update

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+//   dataTable = $("#example").DataTable({
+//     columnDefs: [
+//       {
+//         targets: [5],
+//         visible: false,
+//       },
+//     ],
+//   });
+
+
+
+//   $(".status-dropdown").on("change", function (e) {
+//     var status = $(this).val();
+//     $(".status-dropdown").val(status);
+//     dataTable.column(5).search(status).draw();
+//   });
+});
