7987baf226d1592ef17ef01c2125396dd31f03e7
shoebabedin12 <shoebabedin12@gmail.com>
2023-01-24 22:25:32 +0600

    new page update

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,4 +15,38 @@ $(document).ready(function () {
 //     $(".status-dropdown").val(status);
 //     dataTable.column(5).search(status).draw();
 //   });
+
+
 });
+
+
+
+function strike(e){
+    var data = $(e).attr("class");
+    data = data.substr(3,data.length);
+      if(data == 'fa-caret-square-o-right fa-2x'){
+            $(e).attr("class","fa fa-check-square fa-2x");
+            $(e).parent().parent().css('color', '#ffa500');
+      }else if(data == 'fa-check-square fa-2x'){
+          $(e).parent().parent().css('text-decoration', 'line-through');
+          $(e).parent().parent().css('color', '#008000');
+          $(e).attr("class","fa fa-trash fa-2x");            
+}else{
+  
+      if(confirm("Do you want to delete this list?")){
+          $(e).parent().parent().remove();
+      }
+}
+}
+
+
+function add(){
+var task = $("#task").val();
+var date = $("#date").val();
+
+$(".todo-table").append('<div class="row"><div class="col-lg-6"><div class="mb-5 row"><label for="h-name" class="col-sm-4 col-form-label">Hospital *</label><div class="col-sm-8 position-relative"><input type="text" class="form-control" id="h-name" placeholder="Hospital Name"/></div></div></div><div class="col-lg-6"><div class="mb-5 row"><label for="h-name" class="col-sm-4 col-form-label">Hospital *</label><div class="col-sm-8 position-relative"><input type="text" class="form-control" id="h-name" placeholder="Hospital Name"/></div></div></div><div class="close-icon"><span onclick="strike(this)"></span></div></div>');
+
+
+}
+
+
